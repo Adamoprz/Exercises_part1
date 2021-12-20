92288d422ee4d2c8002964d5d423a2e6962bab57
Adamoprz <adamoprz@gmail.com>
2021-12-20 15:59:31 +0100

    Update3

--- Ex_9_1.py
+++ Ex_9_1.py
@@ -1,5 +1,11 @@
-class Adam:
-    def __init__(self):
-        pass
-
-######
+class Student:
+    #def __init__(self, name : str, index_id : int) -> None:
+    def __init__(self, name, surname, index_id):
+        self.name = name
+        self.index_id = index_id
+        self.surname = surname
+    def __str__(self):
+        return "Student {} {} ma nr indeksu {}".format(self.name, self.surname, self.index_id)
+#
+student = Student("Marin", "Kowalski" , 124)
+print(student)
