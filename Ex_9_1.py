d12ece38d166b66271090361221b08bb5ce3bc5c
Adamoprz <adamoprz@gmail.com>
2021-12-20 22:03:25 +0100

    update4

--- Ex_9_1.py
+++ Ex_9_1.py
@@ -1,11 +1,15 @@
 class Student:
-    #def __init__(self, name : str, index_id : int) -> None:
-    def __init__(self, name, surname, index_id):
+    def __init__(self, name: str, surname: str, index_id: int) -> None:
         self.name = name
         self.index_id = index_id
         self.surname = surname
-    def __str__(self):
-        return "Student {} {} ma nr indeksu {}".format(self.name, self.surname, self.index_id)
-#
-student = Student("Marin", "Kowalski" , 124)
-print(student)
+    def describe_stud(self):
+        print("Student {} {} ma nr indeksu {}".format(self.name, self.surname, self.index_id))
+
+def main():
+    student = Student("Marcin", "Kowalski" , 124)
+    student.describe_stud()
+    print(student.name)
+
+if __name__ == "__main__":
+    main()
