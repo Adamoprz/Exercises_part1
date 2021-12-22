73d4dc158d86f9a900f2a1873f587d6ac0d543e4
Adamoprz <adamoprz@gmail.com>
2021-12-22 17:43:39 +0100

    update7

--- Ex_9_5.py
+++ Ex_9_5.py
@@ -1,17 +1,20 @@
 class Manager:
     def __init__(self) -> None:
         self.orders = {}
-    def add_order(self, order_ob_:str, number_:int) -> None:
-        if order_ob_ in self.orders.keys():
-            self.orders[order_ob_] = self.orders[order_ob_] + number_
+    def add_order(self, a:list, number_:int) -> None:
+        order_ = Order(a)
+        temp_order = order_.return_order()
+        if temp_order in self.orders.keys():
+            self.orders[temp_order] = self.orders[temp_order] + number_
         else:
-            self.order_ob_ = order_ob_
+            self.order_ob_ = temp_order
             self.number_ = number_
             self.orders[self.order_ob_] = self.number_
 
-    def sell(self, id_to_sell: str) -> None:
+    def sell(self, id_to_sell: list) -> None:
         for order in self.orders:
-            if order == id_to_sell:
+            id_to_sell_temp = ((id_to_sell[0],id_to_sell[1],id_to_sell[2]))
+            if order == id_to_sell_temp:
                 self.orders[order] -= 1
 
 
@@ -19,22 +22,22 @@ class Manager:
         print(self.orders)
 
 class Order:
-    def __init__(self, id: int, name: str, price: float) -> None:
-        self.id = id
-        self.name = name
-        self.price = price
-
+    def __init__(self, id_name_price) -> None:
+        self.id = id_name_price[0]
+        self.name = id_name_price[1]
+        self.price = id_name_price[2]
+    def return_order(self):
+        return (self.id, self.name, self.price)
 
 def main():
     new_manager = Manager()
-    new_manager.add_order("kasza" , 10)
-    new_manager.add_order("kasza2", 10)
-    new_manager.add_order("kasza2", 1)
-    new_manager.add_order("kasza2", 7)
+    new_manager.add_order([1, "kasza", 125], 10)
+    new_manager.add_order([2, "kasza2", 125], 10)
+    new_manager.add_order([3, "kasza3", 125], 10)
+    new_manager.add_order([2, "kasza2", 125], 10)
 
     new_manager.show_manager()
-    new_manager.sell("kasza2")
-    new_manager.sell("kasza")
+    new_manager.sell([2, "kasza2", 125])
     new_manager.show_manager()
 
 if __name__ == "__main__":
