746caf1ff9555c76f8f944b537f4cf0f5df45f92
Adamoprz <adamoprz@gmail.com>
2022-01-06 08:44:44 +0100

    update

--- Ex_9_4.py
+++ Ex_9_4.py
@@ -1,10 +1,13 @@
 import random
+# duck typing -- Interpreter sprawdza jakie funkcjonalnosci ma dany obiekt.
+
 
 class Card:
     def __init__(self, value:str, figure:str) -> None:
         self.figure = figure
         self.value = value
-    def return_card(self) -> str:
+
+    def __str__(self) -> str:
         return f"{self.value} - {self.figure}"
 
 
@@ -19,35 +22,40 @@ class Deck:
         for value in self.values:
             for figure in self.figures:
                 t = Card(value, figure)
-                self.add(t.return_card())
+                self.add(t)
 
-    def add(self, added_card: str) -> None:
+    def add(self, added_card: Card) -> None:
         self.card_.append(added_card)
 
     def shuffle(self) -> None:
         random.shuffle(self.card_)
 
     def deal(self) -> str:
-        temp = self.card_[-1]
-        self.card_=self.card_[:-1]
-        return temp
+        #temp = self.card_[-1]
+        #self.card_=self.card_[:-1]
+        return self.card_.pop()
 
 def main():
     figures = ["Diamond", "Clubs", "Heart", "Spade"]
-    values = ["A", "2", "3", "4", "5", "6", "7", "8", "10", "J" , "Q" , "K"]
+    values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J" , "Q" , "K"]
     #Generate and create new deck
     deck_f = Deck(figures, values)
     #deck_f.create_Deck()
     #print all cards of the deck
-    print(deck_f.card_)
+    print(list(deck_f.card_))
     #shuffle deck
     deck_f.shuffle()
     #print shuffled deck
     print(deck_f.card_)
+    print(len(deck_f.card_))
+
     #print last card from the shuffled deck and remove the last card from deck.
     print(deck_f.deal())
     #print deck (now it is reduced by one card
-    print(deck_f.card_)
+    print(deck_f.card_[0])
+    #deck_f.card_[0]
+    #jedna karta mniej
+    print(len(deck_f.card_))
 
 if __name__ == "__main__":
     main()
